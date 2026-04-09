c2e36368940106553d8e84d661c5b500855a7a30
Anna Wilson <annalaurawilson@hotmail.com>
2026-04-09 16:50:15 +0100

    password bcrypt

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,21 +1,30 @@
 const User = require("../models/user");
+const bcrypt = require('bcrypt');
 
-function create(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
 
-  const user = new User({ email, password });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: "Something went wrong" });
-    });
+
+
+
+async function create(req, res) {
+  try {
+    const { email, password: plainTextPassword } = req.body;
+
+    const password = await bcrypt.hash(plainTextPassword, 10);
+
+    const user = new User({ email, password });
+
+    const savedUser = await user.save();
+
+    console.log("User created, id:", savedUser._id.toString());
+    res.status(201).json({ message: "OK" });
+
+  } catch (err) { 
+    console.error("Error in create user:", err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
 }
+
+
 
 const UsersController = {
   create: create,
