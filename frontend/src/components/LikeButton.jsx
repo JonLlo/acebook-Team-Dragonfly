e22da04c229f67ba9f1c52baef27876dd74f2d1e
Anna Wilson <annalaurawilson@hotmail.com>
2026-04-09 10:42:15 +0100

    like button update

--- frontend/src/components/LikeButton.jsx
+++ frontend/src/components/LikeButton.jsx
@@ -1,13 +1,24 @@
-const LikeButton = (props) => {
-        console.log(props);
-        const LikeToggler = () => {
-        props.ToggleYouLike(!props.youLike);
-  };
+// const LikeButton = (props) => {
+//         console.log(props);
+//         const LikeToggler = () => {
+//         props.ToggleYouLike(!props.youLike);
+//   };
 
   
-  return  <button onClick = {LikeToggler} >   {props.youLike ? "❤️ Liked" : "🤍 Like"}</button>
+//   return  <button onClick = {LikeToggler} >   {props.youLike ? "❤️ Liked" : "🤍 Like"}</button>
 
 
 
-}
+// }
+
+
+// LikeButton.jsx
+const LikeButton = ({ youLike, ToggleYouLike }) => {
+  return (
+    <button onClick={() => ToggleYouLike(!youLike)}>
+      {youLike ? "❤️ Liked" : "🤍 Like"}
+    </button>
+  );
+};
+
 export default LikeButton
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -3,28 +3,72 @@ import LikeButton from "./LikeButton";
 import PostInfo from "./PostInfo";
 
 
+// Post.jsx
 const Post = (props) => {
+
+  const [youLike, setYouLike] = useState(false);
+  const [likesCount, setLikesCount] = useState(props.likes?.length || 0);
+
+
+const handleLikeToggle = async () => {
+  const token = localStorage.getItem("token"); 
   
-const [youLike, ToggleYouLike] = useState(false);
+  if (!token) {
+    console.error("No token found in localStorage!");
+    return; 
+  }
 
-
-
+  const newLikeStatus = !youLike;
   
+  setYouLike(newLikeStatus);
+  setLikesCount(newLikeStatus ? likesCount + 1 : likesCount - 1);
 
-
-  console.log(props)
-  return (
-  <div className = "postDiv">
-    <LikeButton youLike = {youLike} ToggleYouLike= {ToggleYouLike} /> 
-    
-    <PostInfo id = {props.id} author_id={props.authorId} author_name = {props.authorName} content = {props.content} img = {props.img} datetime = {props.datetime} comments = {props.comments} time = {props.time} />
-    
-  </div>
-)
-
-
-
-
+  try {
+    await updatePostLike(props._id, newLikeStatus, token); 
+    console.log("Database updated successfully");
+  } catch (err) {
+    setYouLike(!newLikeStatus);
+    setLikesCount(youLike ? likesCount + 1 : likesCount - 1);
+    console.error("Failed to save like:", err);
+  }
 };
+
+  return (
+    <div className="post-container" style={{ borderBottom: "1px solid #eee", padding: "20px" }}>
+      <PostInfo 
+        authorName={props.authorId} 
+        content={props.postContent} 
+        img={props.image} 
+        datetime={props.datetime} 
+      />
+      
+      <div className="like-section">
+        <LikeButton youLike={youLike} ToggleYouLike={handleLikeToggle} />
+        <span>{likesCount} Likes</span>
+      </div>
+    </div>
+  );
+};
+
+// const Post = (props) => {
+  
+// const [youLike, ToggleYouLike] = useState(false);
+
+
+
+  
+
+
+//   console.log(props)
+//   return (
+//   <div className = "postDiv">
+//     <LikeButton youLike = {youLike} ToggleYouLike= {ToggleYouLike} /> 
+    
+//     <PostInfo id = {props.id} author_id={props.authorId} author_name = {props.authorName} content = {props.content} img = {props.img} datetime = {props.datetime} comments = {props.comments} time = {props.time} />
+    
+//   </div>
+// )
+
+// };
 
 export default Post;
