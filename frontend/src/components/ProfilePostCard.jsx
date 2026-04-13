89fda0f8ae17363ab1243b22268a0909bf696eaa
Debra Constantinou <debra.constantinou.tech@gmail.com>
2026-04-13 15:58:29 +0100

    Adds code to implement edit post in the profie page

--- frontend/src/components/ProfilePostCard.jsx
+++ frontend/src/components/ProfilePostCard.jsx
@@ -1,14 +1,73 @@
 //link to package: https://date-fns.org/
-
+import { useState } from "react";
 import { formatDistanceToNow} from "date-fns"
-function ProfilePostCard({ post }) {
-  const formattedDate = new Date(post.createdAt).toLocaleString("en-GB");
-  const timeSincePosted = formatDistanceToNow(new Date(post.createdAt))
+import { editPostContent } from "../services/posts";
+
+
+function ProfilePostCard({ post, onPostUpdated }) {
+  // const formattedDate = new Date(post.createdAt).toLocaleString("en-GB");
+  const timeSincePosted = formatDistanceToNow(new Date(post.createdAt), {addSuffix:true});
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(post.postContent);
+  const [error, setError] = useState("");
+
+  const handleEditClick = ()=> {
+    setEditedContent(post.postContent);
+    setError("");
+    setIsEditing(true);
+  }
+
+  const handleCancel = () => {
+    setEditedContent(post.postContent);
+    setError("");
+    setIsEditing(false);
+  }
+
+  const handleSave = async () => {
+    if(!editedContent.trim()){
+      setError("Post content cannot be blank");
+      return
+    }
+    try{
+      setError("");
+      const data = await editPostContent(post._id,{
+        postContent: editedContent
+      });
+      onPostUpdated(data.post);
+      setIsEditing(false);
+
+    }catch(error){
+      setError(error.message || "Failed to updte post content")
+    }
+  }
+ 
 
   return (
     <article className="post-card">
       <div className="post-card__content">
-        <p>{post.postContent}</p>
+        { isEditing ? (
+         <>
+          <textarea
+            value={editedContent}
+            onChange={(event)=>setEditedContent(event.target.value)}
+            />
+            <div className="post-card__actions">
+              <button type="button" onClick={handleSave}> Save</button>
+              <button type="button" onClick={handleCancel}> Cancel</button>
+            </div>
+            {error && <p>{error}</p>}
+          </>
+        ): (
+        <> 
+          <p>{post.postContent}</p>
+          <div>
+              <button type="button" onClick={handleEditClick}>Edit</button>
+          </div>
+            {error && <p>{error}</p>}
+        </>
+
+      )}
       </div>
 
       {post.postImage && (
@@ -19,18 +78,21 @@ function ProfilePostCard({ post }) {
             alt="Post"
           />
         </div>
+       
+      )}
+        <div className="post-card__meta">
+          <span>{timeSincePosted}</span>
+        </div>
+
+        <div className="post-card__stats">
+          <span>Likes: {post.likesCount}</span>
+          <span>Comments: {post.commentsCount}</span>
+        </div>
+        
+        
+        </article>
+        
       )}
 
-      <div className="post-card__meta">
-        <span>{timeSincePosted}</span>
-      </div>
-
-      <div className="post-card__stats">
-        <span>Likes: {post.likesCount}</span>
-        <span>Comments: {post.commentsCount}</span>
-      </div>
-    </article>
-  );
-}
 
 export default ProfilePostCard;
