b01ee6bf2ea3365c97664f02b52aa3dd27fefa5b
Anna Wilson <annalaurawilson@hotmail.com>
2026-04-14 17:50:32 +0100

    create post name, some styling and fixed loop

--- frontend/src/components/PostInfo.jsx
+++ frontend/src/components/PostInfo.jsx
@@ -1,43 +1,23 @@
-// function Post(props) {
-//   return <article key={props.post._id}>{props.post.message}</article>;
-// }
-
-// // PostInfo.jsx
-// const PostInfo = (props) => {
-//   return (
-//     <div className="aPost">
-//       <h3 className="authorName">{props.authorName}</h3>
-//       <p className="postContent">{props.content}</p>
-//       {props.img && (
-//         <img className="postImg" src={props.img} alt="Post content" />
-//       )}
-//       <p className="datetime">
-//         <small>{props.datetime}</small>
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default PostInfo;
-
+import "./PostInfo.css";
 
 const PostInfo = (props) => {
   // We need to extract the string values from the object
   // We use "Optional Chaining" (?.) to prevent crashes if authorName is missing
-  const displayName = props.authorName?.firstName 
-    ? `${props.authorName.firstName} ${props.authorName.surname}` 
+  const displayName = props.author?.firstName 
+    ? `${props.author.firstName} ${props.author.surname}` 
     : "Unknown Author";
 
   return (
+    
     <div className="aPost">
-      {/* Before: {props.authorName} - This caused the crash */}
+      <div className="postHeader">
+        {props.author.userImage && (
+        <img className="postImg" src={props.author.userImage} alt="Post content" style={{ width: "80px", height: "80px", borderRadius: "50%" }}/>
+      )}
       <h3 className="authorName">{displayName}</h3>
+      </div>
       
       <p className="postContent">{props.content}</p>
-      
-      {props.img && (
-        <img className="postImg" src={props.img} alt="Post content" />
-      )}
       
       <p className="datetime">
         <small>{props.datetime}</small>
