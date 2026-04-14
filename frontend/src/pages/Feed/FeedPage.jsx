f80c6c148ea1b28ec6353afa4ce8f6d4161e7e86
Jonny <jonnylloydie1@gmail.com>
2026-04-14 15:42:16 +0100

    small change

--- frontend/src/pages/Feed/FeedPage.jsx
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -1,32 +1,27 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { getPosts } from "../../services/posts";
 import Post from "../../components/Post";
 // import LogoutButton from "../../components/LogoutButton";
 import "./FeedPage.css";
 import Navbar from "../../components/Navbar";
 import CreatePost from "../../components/CreatePost";
-
 export function FeedPage() {
-  //   let hardcoded_post = {
-
-  //   postContent: "this is a post",
-  //   authorImage: "../public/images/defaultAvatar",
-  //   comments: {"com_test": "com_test2"},
-  //   likes: {"like_test": "like_test2"},
+  //   let hardcoded_post = {
+  //   postContent: "this is a post",
+  //   authorImage: "../public/images/defaultAvatar",
+  //   comments: {"com_test": "com_test2"},
+  //   likes: {"like_test": "like_test2"},
   // }
-  //   let hardcoded_post_2 = {
-
-  //   postContent: "this is a post",
-  //   authorImage: "../public/images/defaultAvatar",
-  //   comments: {"com_test": "com_test2"},
-  //   likes: {"like_test": "like_test2"},
+  //   let hardcoded_post_2 = {
+  //   postContent: "this is a post",
+  //   authorImage: "../public/images/defaultAvatar",
+  //   comments: {"com_test": "com_test2"},
+  //   likes: {"like_test": "like_test2"},
   // }
-
-  const [posts, setPosts] = useState([]); //console.log(`HERE! POSTS:` +  posts)
+  const [posts, setPosts] = useState([]);
+  //console.log(`HERE! POSTS:` +  posts)
   const navigate = useNavigate();
-
   useEffect(() => {
     const token = localStorage.getItem("token");
     const loggedIn = token !== null;
@@ -35,8 +30,8 @@ export function FeedPage() {
         .then((data) => {
           setPosts(data.posts);
           localStorage.setItem("token", data.token);
-          console.log("TOKENTOKEN", data.token);
-          console.log("POSTPOST", data.posts);
+          console.log("TOKENTOKEN", data.token)
+          console.log("POSTPOST", data.posts)
         })
         .catch((err) => {
           console.error(err);
@@ -44,40 +39,33 @@ export function FeedPage() {
         });
     }
   }, [navigate]);
-
   const token = localStorage.getItem("token");
   if (!token) {
     navigate("/login");
     return;
   }
-
   return (
     <>
-          
-      <Navbar />
-          
-      <div className="feed-content">
-              <h2>My Feed</h2>
-              
-        <CreatePost />
-              
-        <div className="feed" role="feed">
-                  
-          {posts.map((post, index) => {
-            console.log(post._id); //console.log("HERE POSTS 2:", posts);
-            console.log("INDEX: " + index + " POST: " + post.postContent);
-            console.log("LIKES " + post.likesCount); //fetch request fetching the count and the status of youLike. need to send the token and the
-            // parameter: post_id
-            // token : user_id in the header
-            // Only render the Post component if there is actually content
-
-            return post.postContent ? <Post {...post} key={index} /> : null;
-          })}
-                
-        </div>
-            
+    <Navbar />
+    <div className="feed-content">
+      <h2>My Feed</h2>
+      <CreatePost />
+      <div className="feed" role="feed">
+        {posts.map((post, index) => {
+          console.log(post._id)
+          //console.log("HERE POSTS 2:", posts);
+          console.log("INDEX: " + index + " POST: " + post.postContent);
+          console.log("LIKES " + post.likesCount);
+          //fetch request fetching the count and the status of youLike. need to send the token and the
+          // parameter: post_id
+          // token : user_id in the header
+        // Only render the Post component if there is actually content
+        return post.postContent ? (
+          <Post {...post} key={index} />
+        ) : null;
+      })}
       </div>
-          
+    </div>
     </>
   );
 }
