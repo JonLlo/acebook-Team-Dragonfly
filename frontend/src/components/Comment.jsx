20ca0707deb993fc314cdfc039516d239f900873
Jonny <jonnylloydie1@gmail.com>
2026-04-14 17:21:55 +0100

    Comments now add with name

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -2,8 +2,8 @@ const Comment = (props) => {
   return (
     <div>
       {props.comments && props.comments.length > 0 ? (
-        props.comments.map((comment) =>
-          comment.commentContent ? (
+        props.comments.map((comment) => {
+          return comment.commentContent ? (
             <div
               key={comment._id}
               className="comment"
@@ -13,19 +13,22 @@ const Comment = (props) => {
                 src={comment.author?.userImage}
                 alt="avatar"
                 style={{ width: "40px", height: "40px", borderRadius: "50%" }}
-              />{" "}
+              />
               <p>
                 {comment.author.firstName} {" - "}
                 {comment.commentContent}
               </p>
             </div>
-          ) : null,
-        )
+          ) : null;
+        })
       ) : (
         <p>No comments yet.</p>
       )}
     </div>
   );
 };
+
+
+
 
 export default Comment;
