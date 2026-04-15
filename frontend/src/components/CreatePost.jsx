4d12dcaa8ff90fc871fd29fc9a72ccaf50f63cf7
Alex Tanner <alextanner@fastmail.com>
2026-04-15 12:23:13 +0100

    changes CreatePost by removing extra fetch request

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import "./CreatePost.css";
-
-
 
 
 const CreatePost = (props) => {
@@ -10,7 +7,6 @@ const CreatePost = (props) => {
     const [postImage, setPostImage] = useState("");
     const [posts, setPosts] = useState(props.posts || []);
 
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("http://localhost:3000/posts")
@@ -60,30 +56,7 @@ const CreatePost = (props) => {
                 console.log("Post failed error:", error);
             });
     };
-    fetch("http://localhost:3000/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/JSON",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(postInfo),
-    })
-      .then((res) => {
-        if (res.status === 401 || res.status === 403) {
-          throw new Error("your session expired. Please log in again.");
-        }
-        return res.json();
-      })
-      .then((newPost) => {
-        setPostContent("");
-        setPostImage("");
-
-        navigate("/posts");
-      })
-      .catch((error) => {
-        console.log("Post failed error:", error);
-      });
-  };
+    
   return (
     <div className="createPost">
       <div className="submitForm">
